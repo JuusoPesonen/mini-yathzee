ce9e03efeea88d4c7801fbbe23f1c6bd73778fce
JuusoPesonen <c2peju00@students.oamk.fi>
2024-02-27 21:02:47 +0200

    Changed code on project

--- mini-yatzi/App.js
+++ mini-yatzi/App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from "./Components/Home";
+import Scoreboard from "./Components/Scoreboard";
+import { View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import Gameboard from "./Components/Gameboard";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        sceneContainerStyle={{ backgroundColor: "Transparent" }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home";
+            } else if (route.name === "Gameboard") {
+              iconName = focused ? "gamepad" : "gamepad";
+            } else if (route.name === "Scoreboard") {
+              iconName = focused ? "pencil" : "pencil";
+            }
+
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "#957700",
+          tabBarInactiveTintColor: "#000000",
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} options={{ tabBarStyle: { display: "none" } }} />
+        <Tab.Screen name="Gameboard" component={Gameboard} />
+        <Tab.Screen name="Scoreboard" component={Scoreboard} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
